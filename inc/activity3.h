67750afa813f542e7ac9152b48d5fd277cd908b0
256323 <70212456+malavika-m@users.noreply.github.com>
2021-04-30 16:08:20 +0530

    Add files via upload

---
+++ inc/activity3.h
@@ -0,0 +1,6 @@
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+
+void compare(uint16_t temp);
+
+#endif // ACTIVITY3_H_INCLUDED
---
+++ inc/activity4.h
@@ -0,0 +1,6 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
+
+void USART_init();
+void display(uint16_t temp);
+#endif // ACTIVITY4_H_INCLUDED
